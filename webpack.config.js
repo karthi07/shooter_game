dad3a09c087c8ab1d4ed918b762b7fcef22e4cb0
karthi07 <itkarthi02@gmail.com>
2020-07-19 01:59:05 +0530

    configure webpack and add phaser and test it

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+  }
+};
